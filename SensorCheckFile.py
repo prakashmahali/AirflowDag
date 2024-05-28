69d98e9ae37390ab56eb9a87365ee388e155b395
prakashmahali <prakash.mahali@gmail.com>
2024-05-28 10:37:57 +0530

    Update SensorCheckFile.py

--- SensorCheckFile.py
+++ SensorCheckFile.py
@@ -1,141 +1,52 @@
 from airflow import DAG
-from airflow.providers.google.cloud.sensors.gcs import GCSObjectExistenceSensor
-from airflow.operators.python_operator import BranchPythonOperator
+from airflow.providers.google.cloud.operators.gcs import GoogleCloudStorageListOperator
 from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
+from airflow.models import Variable
+
+def check_file_exists(**kwargs):
+    bucket_name = Variable.get('gcs_bucket_name')
+    file_name = "test.csv"  # File to check for existence
+
+    gcs_hook = kwargs['ti'].xcom_pull(task_ids='list_gcs_files')
+    file_list = gcs_hook['blobs']
+
+    for file in file_list:
+        if file.name == file_name:
+            return 'file_exists'
+    return 'file_not_found'
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'start_date': days_ago(1),
-    'retries': 1,
 }
 
-BUCKET_NAME = 'your-bucket-name'
-FILE_NAME = 'path/to/your/file.txt'
-
-def choose_task(**kwargs):
-    ti = kwargs['ti']
-    file_exists = ti.xcom_pull(task_ids='check_file')
-    if file_exists:
-        return 'task1'
-    else:
-        return 'task2'
-
 with DAG(
-    dag_id='example_gcs_file_check_dag',
+    dag_id='check_gcs_file_existence',
     default_args=default_args,
     schedule_interval=None,
+    catchup=False
 ) as dag:
 
-    start = DummyOperator(task_id='start')
-
-    check_file = GCSObjectExistenceSensor(
-        task_id='check_file',
-        bucket=BUCKET_NAME,
-        object=FILE_NAME,
+    # Task to list files in GCS bucket
+    list_gcs_files = GoogleCloudStorageListOperator(
+        task_id='list_gcs_files',
+        bucket="{{ var.value.gcs_bucket_name }}",
     )
 
-    branch = BranchPythonOperator(
-        task_id='branch',
+    # Task to check if file exists
+    check_file_task = PythonOperator(
+        task_id='check_file_task',
+        python_callable=check_file_exists,
         provide_context=True,
-        python_callable=choose_task,
     )
 
-    task1 = DummyOperator(task_id='task1')
-    task2 = DummyOperator(task_id='task2')
-    end = DummyOperator(task_id='end', trigger_rule='none_failed_or_skipped')
+    # Dummy tasks for conditional execution
+    file_exists_task = DummyOperator(task_id='file_exists_task')
+    file_not_found_task = DummyOperator(task_id='file_not_found_task')
 
-    start >> check_file >> branch
-    branch >> [task1, task2] >> end
-
-Step 1 : 
-check_file = GCSObjectExistenceSensor(
-    task_id='check_file',
-    bucket=BUCKET_NAME,
-    object=FILE_NAME,
-)
- Step2 
-BranchPythonOperator: Uses the choose_task function to decide which task to execute next based on the file's existence.
-def choose_task(**kwargs):
-    ti = kwargs['ti']
-    file_exists = ti.xcom_pull(task_ids='check_file')
-    if file_exists:
-        return 'task1'
-    else:
-        return 'task2'
-Task Flow:
-
-start: Initial dummy task.
-check_file: Sensor task to check for the file's existence.
-branch: Branching based on the sensor result.
-task1: Dummy task executed if the file exists.
-task2: Dummy task executed if the file does not exist.
-end: Final task to ensure the DAG completes successfully.
-############################# Option 2 #########################################################################
-                                from airflow import DAG
-from airflow.providers.google.cloud.sensors.gcs import GCSObjectExistenceSensor
-from airflow.providers.google.cloud.transfers.gcs_to_gcs import GCSToGCSOperator
-from airflow.operators.python_operator import BranchPythonOperator
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.utils.dates import days_ago
-from airflow.utils.trigger_rule import TriggerRule
-
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': days_ago(1),
-    'retries': 1,
-}
-
-SOURCE_BUCKET = 'your-source-bucket-name'
-DESTINATION_BUCKET = 'your-destination-bucket-name'
-FILE_NAME = 'path/to/your/file.txt'
-DESTINATION_FILE_NAME = FILE_NAME  # You can change the destination path if needed
-
-def choose_task(**kwargs):
-    ti = kwargs['ti']
-    file_exists = ti.xcom_pull(task_ids='check_file')
-    if file_exists:
-        return 'task1'
-    else:
-        return 'task2'
-
-with DAG(
-    dag_id='gcs_to_gcs_file_copy_dag',
-    default_args=default_args,
-    schedule_interval=None,
-) as dag:
-
-    start = DummyOperator(task_id='start')
-
-    check_file = GCSObjectExistenceSensor(
-        task_id='check_file',
-        bucket=SOURCE_BUCKET,
-        object=FILE_NAME,
-    )
-
-    copy_file = GCSToGCSOperator(
-        task_id='copy_file',
-        source_bucket=SOURCE_BUCKET,
-        source_object=FILE_NAME,
-        destination_bucket=DESTINATION_BUCKET,
-        destination_object=DESTINATION_FILE_NAME,
-        move_object=False,  # Set to True if you want to move instead of copy
-    )
-
-    branch = BranchPythonOperator(
-        task_id='branch',
-        provide_context=True,
-        python_callable=choose_task,
-    )
-
-    task1 = DummyOperator(task_id='task1')
-    task2 = DummyOperator(task_id='task2')
-    end = DummyOperator(task_id='end', trigger_rule=TriggerRule.NONE_FAILED_MIN_ONE_SUCCESS)
-
-    start >> check_file >> branch
-    branch >> copy_file >> task1 >> end
-    branch >> task2 >> end
-
-       
+    # Define DAG structure
+    list_gcs_files >> check_file_task
+    check_file_task >> [file_exists_task, file_not_found_task]
